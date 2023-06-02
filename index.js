e4b08307259301546bfcda004602e842a88d4886
Rodrigo Grapeia <r.grapeia@ecotrace.info>
2023-06-02 02:07:33 -0300

    prettier

--- index.js
+++ index.js
@@ -1,46 +1,50 @@
 async function ytgGetVideoConfig() {
-    return new Promise((resolve) => {
-        setTimeout(async () => {
-            const html = await ytgFetchContent();
-            startStr = "ytInitialPlayerResponse =";
-            start = html.indexOf(startStr) + startStr.length;
-            end = html.indexOf("};", start) + 1;
-            resolve(JSON.parse(html.slice(start, end)));
-        }, 1000);
-    });
+	return new Promise((resolve) => {
+		setTimeout(async () => {
+			const html = await ytgFetchContent();
+			startStr = "ytInitialPlayerResponse =";
+			start = html.indexOf(startStr) + startStr.length;
+			end = html.indexOf("};", start) + 1;
+			resolve(JSON.parse(html.slice(start, end)));
+		}, 1000);
+	});
 }
 
 async function ytgSetDownloadURL(stream) {
-    if (stream.url) return stream.url;
-    if (typeof window.ytgWinDecipher !== "function") await ytgUpdateDecipherInstance(html);
-    const params = ytgGetParams(stream.signatureCipher || stream.cipher);
-    const url = new URL(params.url);
-    url.searchParams.set(params.sp ? params.sp : 'signature', ytgDecipher(params.s));
-    return url.toString();
-};
+	if (stream.url) return stream.url;
+	if (typeof window.ytgWinDecipher !== "function")
+		await ytgUpdateDecipherInstance(html);
+	const params = ytgGetParams(stream.signatureCipher || stream.cipher);
+	const url = new URL(params.url);
+	url.searchParams.set(
+		params.sp ? params.sp : "signature",
+		ytgDecipher(params.s)
+	);
+	return url.toString();
+}
 
 function ytgGetParams(url) {
-    const params = {};
-    const parser = document.createElement('a');
-    parser.href = 'youtube.com?' + url;
-    const query = parser.search.substring(1);
-    const vars = query.split('&');
-    for (let i = 0; i < vars.length; i++) {
-        const pair = vars[i].split('=');
-        params[pair[0]] = decodeURIComponent(pair[1]);
-    }
-    return params;
+	const params = {};
+	const parser = document.createElement("a");
+	parser.href = "youtube.com?" + url;
+	const query = parser.search.substring(1);
+	const vars = query.split("&");
+	for (let i = 0; i < vars.length; i++) {
+		const pair = vars[i].split("=");
+		params[pair[0]] = decodeURIComponent(pair[1]);
+	}
+	return params;
 }
 
 function ytgDownloadVideo({
-    title,
-    videoFileName,
-    url,
-    thumbnail,
-    cipher = false
+	title,
+	videoFileName,
+	url,
+	thumbnail,
+	cipher = false,
 }) {
-    const hostname = window.location.hostname;
-    const html = `
+	const hostname = window.location.hostname;
+	const html = `
         <html>
             <head>
                 <title>Downloading ${title}</title>
@@ -164,180 +168,210 @@ function ytgDownloadVideo({
             </script>
             </body>
         </html >`;
-    const d = new Date();
-    const popup = window.open(
-        d,
-        "_blank",
-        "width=650,height=400,left=0,top=0,location=no,scrollbars=yes,resizable=yes"
-    );
-    popup.document.write(html);
+	const d = new Date();
+	const popup = window.open(
+		d,
+		"_blank",
+		"width=650,height=400,left=0,top=0,location=no,scrollbars=yes,resizable=yes"
+	);
+	popup.document.write(html);
 }
 
 async function ytgAddButtonsToPlayerControls() {
-    const config = await ytgGetVideoConfig();
-    const includedControl = [];
-    const streams = config.streamingData.adaptiveFormats.filter((format) => {
-        const mustInclude = format.mimeType.includes("video/mp4") && [1080, 720, 480].includes(format.height) && !includedControl.includes(format.height);
-        includedControl.push(format.height);
-        return mustInclude;
-    });
-    streams.forEach(async (stream) => {
-        const button = document.createElement("button");
-        const videoFileName = `${config.videoDetails.title}-${stream.qualityLabel}.mp4`;
-        const thumbnail =
-            stream.thumbnail ||
-            config.videoDetails.thumbnail.thumbnails[
-                config.videoDetails.thumbnail.thumbnails.length - 1
-            ].url;
-        button.innerHTML = `<span class="ytp-button-content">${stream.qualityLabel}</span>`;
-        button.className = "ytp-button ytp-download-button";
-        button.style =
-            "background-color: #fff; border: 1px solid #ccc; border-radius: 3px; color: #000; font-size: 11px; font-weight: bold; height: 24px; line-height: 24px; margin-left: 5px; padding: 0 10px;";
+	const config = await ytgGetVideoConfig();
+	const includedControl = [];
+	const streams = config.streamingData.adaptiveFormats.filter((format) => {
+		const mustInclude =
+			format.mimeType.includes("video/mp4") &&
+			[1080, 720, 480].includes(format.height) &&
+			!includedControl.includes(format.height);
+		includedControl.push(format.height);
+		return mustInclude;
+	});
+	streams.forEach(async (stream) => {
+		const button = document.createElement("button");
+		const videoFileName = `${config.videoDetails.title}-${stream.qualityLabel}.mp4`;
+		const thumbnail =
+			stream.thumbnail ||
+			config.videoDetails.thumbnail.thumbnails[
+				config.videoDetails.thumbnail.thumbnails.length - 1
+			].url;
+		button.innerHTML = `<span class="ytp-button-content">${stream.qualityLabel}</span>`;
+		button.className = "ytp-button ytp-download-button";
+		button.style =
+			"background-color: #fff; border: 1px solid #ccc; border-radius: 3px; color: #000; font-size: 11px; font-weight: bold; height: 24px; line-height: 24px; margin-left: 5px; padding: 0 10px;";
 
-        const url = await ytgSetDownloadURL(stream)
-        button.onclick = () => {
-            ytgDownloadVideo({
-                title: config.videoDetails.title,
-                videoFileName,
-                url,
-                thumbnail,
-                cipher: !!stream.url
-            });
-        };
-        const playButton = document.getElementsByClassName("ytp-time-display")[0];
-        button.style.margin = "10px";
-        playButton.parentNode.insertBefore(button, playButton.nextSibling);
-    });
+		const url = await ytgSetDownloadURL(stream);
+		button.onclick = () => {
+			ytgDownloadVideo({
+				title: config.videoDetails.title,
+				videoFileName,
+				url,
+				thumbnail,
+				cipher: !!stream.url,
+			});
+		};
+		const playButton = document.getElementsByClassName("ytp-time-display")[0];
+		button.style.margin = "10px";
+		playButton.parentNode.insertBefore(button, playButton.nextSibling);
+	});
 
-    const allStreams = [...config.streamingData.adaptiveFormats].filter(Boolean);
-    const select = document.createElement("select");
-    select.className = "ytp-download-button";
-    select.style =
-        "background-color: #fff; border: 1px solid #ccc; border-radius: 3px; color: #000; font-size: 11px; font-weight: bold; height: 24px; line-height: 24px; margin-left: 5px; padding: 0 10px;";
-    select.onchange = async () => {
-        const selectedStream = allStreams[select.value];
-        const videoFileName = `${config.videoDetails.title}-${ytgIsVideo(selectedStream) ? "video" + selectedStream.qualityLabel : "audio"}-${selectedStream.bitrate}kbps.${ytgGetFileExtension(selectedStream)}`;
-        const thumbnail =
-            selectedStream.thumbnail ||
-            config.videoDetails.thumbnail.thumbnails[
-                config.videoDetails.thumbnail.thumbnails.length - 1
-            ].url;
-        const url = await ytgSetDownloadURL(selectedStream)
-        ytgDownloadVideo({
-            title: config.videoDetails.title,
-            videoFileName,
-            url,
-            thumbnail,
-            cipher: !!selectedStream.url
-        });
-    };
-    const option = document.createElement("option");
-    option.value = "";
-    option.innerHTML = "All Streams download";
-    select.appendChild(option);
-    allStreams.forEach((stream, i) => {
-        const option = document.createElement("option");
-        option.value = i;
-        if (ytgIsVideo(stream))
-            option.innerHTML = ytgGetType(stream) + " - " + stream.qualityLabel + ", " + stream.fps + "fps, " + ytgGetReadableBitrate(stream);
-        else
-            option.innerHTML = ytgGetType(stream) + " - " + ytgGetReadableBitrate(stream);
-        select.appendChild(option);
-    });
+	const allStreams = [...config.streamingData.adaptiveFormats].filter(Boolean);
+	const select = document.createElement("select");
+	select.className = "ytp-download-button";
+	select.style =
+		"background-color: #fff; border: 1px solid #ccc; border-radius: 3px; color: #000; font-size: 11px; font-weight: bold; height: 24px; line-height: 24px; margin-left: 5px; padding: 0 10px;";
+	select.onchange = async () => {
+		const selectedStream = allStreams[select.value];
+		const videoFileName = `${config.videoDetails.title}-${
+			ytgIsVideo(selectedStream)
+				? "video" + selectedStream.qualityLabel
+				: "audio"
+		}-${selectedStream.bitrate}kbps.${ytgGetFileExtension(selectedStream)}`;
+		const thumbnail =
+			selectedStream.thumbnail ||
+			config.videoDetails.thumbnail.thumbnails[
+				config.videoDetails.thumbnail.thumbnails.length - 1
+			].url;
+		const url = await ytgSetDownloadURL(selectedStream);
+		ytgDownloadVideo({
+			title: config.videoDetails.title,
+			videoFileName,
+			url,
+			thumbnail,
+			cipher: !!selectedStream.url,
+		});
+	};
+	const option = document.createElement("option");
+	option.value = "";
+	option.innerHTML = "All Streams download";
+	select.appendChild(option);
+	allStreams.forEach((stream, i) => {
+		const option = document.createElement("option");
+		option.value = i;
+		if (ytgIsVideo(stream))
+			option.innerHTML =
+				ytgGetType(stream) +
+				" - " +
+				stream.qualityLabel +
+				", " +
+				stream.fps +
+				"fps, " +
+				ytgGetReadableBitrate(stream);
+		else
+			option.innerHTML =
+				ytgGetType(stream) + " - " + ytgGetReadableBitrate(stream);
+		select.appendChild(option);
+	});
 
-    const nextButton2 = document.getElementsByClassName("ytp-time-display")[0];
-    select.style.margin = "10px";
-    nextButton2.parentNode.insertBefore(select, nextButton2.nextSibling);
+	const nextButton2 = document.getElementsByClassName("ytp-time-display")[0];
+	select.style.margin = "10px";
+	nextButton2.parentNode.insertBefore(select, nextButton2.nextSibling);
 }
 
 function ytgGetType(stream) {
-    return String(stream.mimeType.split(";")[0]).toUpperCase();
+	return String(stream.mimeType.split(";")[0]).toUpperCase();
 }
 
 function ytgIsVideo(stream) {
-    return stream.mimeType.includes("video");
+	return stream.mimeType.includes("video");
 }
 
 function ytgGetFileExtension(stream) {
-    return stream.mimeType.split(";")[0].split("/")[1];
+	return stream.mimeType.split(";")[0].split("/")[1];
 }
 
 function ytgGetAudioCodec(stream) {
-    return stream.mimeType.split(";")[1].split("=")[1].replace(`"`, "");
+	return stream.mimeType.split(";")[1].split("=")[1].replace(`"`, "");
 }
 
 function ytgGetReadableBitrate(stream) {
-    const bitrate = stream.bitrate;
-    if (bitrate < 1000) {
-        return bitrate + " Kbps";
-    } else {
-        return (bitrate / 1000).toFixed(2) + " Mbps";
-    }
+	const bitrate = stream.bitrate;
+	if (bitrate < 1000) {
+		return bitrate + " Kbps";
+	} else {
+		return (bitrate / 1000).toFixed(2) + " Mbps";
+	}
 }
 
 function ytgRemoveButtonsFromPlayerControls() {
-    const buttons = document.getElementsByClassName("ytp-download-button");
-    while (buttons.length > 0) {
-        buttons[0].remove();
-    }
+	const buttons = document.getElementsByClassName("ytp-download-button");
+	while (buttons.length > 0) {
+		buttons[0].remove();
+	}
 }
 
 function ytgBetweenText(content, left, right) {
-    let pos;
-    if (left instanceof RegExp) {
-        const match = content.match(left);
-        if (!match) { return ''; }
-        pos = match.index + match[0].length;
-    } else {
-        pos = content.indexOf(left);
-        if (pos === -1) { return ''; }
-        pos += left.length;
-    }
-    content = content.slice(pos);
-    pos = content.indexOf(right);
-    if (pos === -1) { return ''; }
-    content = content.slice(0, pos);
-    return content;
-};
+	let pos;
+	if (left instanceof RegExp) {
+		const match = content.match(left);
+		if (!match) {
+			return "";
+		}
+		pos = match.index + match[0].length;
+	} else {
+		pos = content.indexOf(left);
+		if (pos === -1) {
+			return "";
+		}
+		pos += left.length;
+	}
+	content = content.slice(pos);
+	pos = content.indexOf(right);
+	if (pos === -1) {
+		return "";
+	}
+	content = content.slice(0, pos);
+	return content;
+}
 
 async function ytgFetchContent(url = window.location.href) {
-    return await fetch(url)
-        .then((resp) => resp.text())
-        .then((text) => text);
+	return await fetch(url)
+		.then((resp) => resp.text())
+		.then((text) => text);
 }
 
 async function ytgUpdateDecipherInstance(html) {
-    const htmlToParse = html || await ytgFetchContent();
-    const html5player = ytgGetPlayerJsUrl(htmlToParse);
-    const jsContent = await ytgFetchContent(html5player);
-    const functionName = ytgBetweenText(jsContent, `a.set("alr","yes");c&&(c=`, `(decodeURIC`);
-    const toEval = jsContent.replace(`${functionName}=function(a)`, `window.ytgWinDecipher=function(a)`).replace('_yt_player', 'grapsPlayer');
-    eval(toEval);
+	const htmlToParse = html || (await ytgFetchContent());
+	const html5player = ytgGetPlayerJsUrl(htmlToParse);
+	const jsContent = await ytgFetchContent(html5player);
+	const functionName = ytgBetweenText(
+		jsContent,
+		`a.set("alr","yes");c&&(c=`,
+		`(decodeURIC`
+	);
+	const toEval = jsContent
+		.replace(`${functionName}=function(a)`, `window.ytgWinDecipher=function(a)`)
+		.replace("_yt_player", "grapsPlayer");
+	eval(toEval);
 }
 
 function ytgDecipher(s) {
-    return window.ytgWinDecipher(s);
+	return window.ytgWinDecipher(s);
 }
 
 function ytgGetPlayerJsUrl(html) {
-    const match = /<script\s+src="([^"]+)"(?:\s+type="text\/javascript")?\s+name="player_ias\/base"\s*>|"jsUrl":"([^"]+)"/.exec(html);
-    const path = match ? match[1] || match[2] : null;
-    return new URL(path, window.location.href).href;
+	const match =
+		/<script\s+src="([^"]+)"(?:\s+type="text\/javascript")?\s+name="player_ias\/base"\s*>|"jsUrl":"([^"]+)"/.exec(
+			html
+		);
+	const path = match ? match[1] || match[2] : null;
+	return new URL(path, window.location.href).href;
 }
 
 function ytgCallProcess() {
-    ytgUpdateDecipherInstance();
-    ytgRemoveButtonsFromPlayerControls();
-    ytgAddButtonsToPlayerControls();
+	ytgUpdateDecipherInstance();
+	ytgRemoveButtonsFromPlayerControls();
+	ytgAddButtonsToPlayerControls();
 }
 
 function ytgStartProcess() {
-    if (!window.location.href.includes("watch?v=")) {
-        setTimeout(ytgStartProcess, 1000);
-        return;
-    }
-    // listener event when youtube player is ready
-    document.addEventListener('yt-navigate-finish', ytgCallProcess);
-    ytgCallProcess();
+	if (!window.location.href.includes("watch?v=")) {
+		setTimeout(ytgStartProcess, 1000);
+		return;
+	}
+	// listener event when youtube player is ready
+	document.addEventListener("yt-navigate-finish", ytgCallProcess);
+	ytgCallProcess();
 }
 ytgStartProcess();
